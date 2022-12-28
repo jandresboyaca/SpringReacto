d600e80ce97f10340e93b587d097fef8a97db34f
Andres Boyaca <j.boyaca@uniandes.edu.co>
2022-12-28 18:38:34 -0500

    stylish cast to Object[]

--- src/main/java/com/example/reactor/CleanService.java
+++ src/main/java/com/example/reactor/CleanService.java
@@ -3,22 +3,15 @@ package com.example.reactor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
 @Service
 public class CleanService {
 
+
     public void test(List<ResourceDTO> resourceDTOS) {
         log.info(resourceDTOS.toString());
     }
 
-    public void test(ResourceDTO[] resourceDTOS) {
-        log.info(Arrays.toString(resourceDTOS));
-    }
-
-    public void test(Object[] objects) {
-        log.info(Arrays.toString(objects));
-    }
 }
--- src/main/java/com/example/reactor/ReactorApplication.java
+++ src/main/java/com/example/reactor/ReactorApplication.java
@@ -29,11 +29,11 @@ public class ReactorApplication implements CommandLineRunner {
 
         log.info("EXECUTING : command line runner");
 
-        List<Mono<ResourceDTO>> requests = IntStream.rangeClosed(1, 5)
+        List<Mono<ResourceDTO>> requests = IntStream.rangeClosed(1, 2)
                 .mapToObj(webClient::test)
                 .collect(Collectors.toList());
 
-        Mono.zip(requests, objects -> objects)
+        Mono.zip(requests, objects -> Arrays.stream(objects).map(ResourceDTO.class::cast).collect(Collectors.toList()))
                 .doOnSuccess(cleanService::test)
                 .doOnError(throwable -> log.error(throwable.getMessage()))
                 .block();
